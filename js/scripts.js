7a3ba0bba3da6f90a6eaf4b7e5dca6ce0c559a47
aleezay <aleezayh@hotmail.com>
2017-07-26 09:32:16 -0700

    included a fork to spoon-knife

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json';
        url += '?' + $.param({'api-key': '53dfced34ab7497ca7331f1f197cb367'
      })
+    
+    $('.stories').empty(); //or .remove
+
+    // $(".ajax-loader").show();   
+
     $.ajax({
       method: 'GET',
       url: url
@@ -17,40 +22,34 @@ $(document).ready(function() {
       var articleWithImage = data.results.filter(function(value){ //data.results.filter?
                     return value.multimedia.length
       }).slice(0, 12);
-     
-      // var articleImage = value.multimedia[4].url, 
-      //     abstract = value.abstract,
-      //     articleUrl = value.url; 
-
-    // var results = data.results;
-    // var articleImage, abstract, articleUrl,
-    // articleImage = results.filter(function(value){ //data.results.filter?
-    //                 return value.multimedia.length > 0;
-    // });//end of .filter
 
      $.each(articleWithImage, function(key,value){
 
-      //  if (value.multimedia.length > 0 ) {
-    
-    
-          articles += '<ul>';
-          articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<img src="' + value.multimedia[4].url + '" id="news-img"/>' + '</a>';
-          articles += '<p class="abstract">' + value.abstract + '</p>'
-          articles += '</ul>';
-          
-          //results.slice(0,12)
-   
+        articles += '<ul class="articles">';
+        articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<img class="image" src="' + value.multimedia[4].url + '" id="news-img"/>' + '</a>';
+        articles += '<p class="abstract">' + value.abstract + '</p>'
+        articles += '</ul>';
+
+      //  articles += '<ul class="news">';
+      //  articles += '<a href=' + value.url + '>';
+      //  articles += '<div class="article" style="background-image: url(' + value.multimedia[4].url + ')">';
+      //  articles += '<p class="abstract">' + value.abstract + '</p>'
+      //  articles += '</p></div></a></ul>';    
+      // });
+
      });//end of .each
 
-// $('.stories ul').append('<li><a href="(' + articleUrl +')"><article style="background-image:url(' + articleImage + ')">' + '<div class="overlay">' + '<p>' + abstract + '</p>' + '</div>' + '</article></a></li>');
-
 $('.stories').append(articles);
-//$('.stories ul').append('<li>hi</li>');
 
-});// end of done
-  
-  //   //  $(".stories ul").append("<li><img 
-  //   // src='"+value.artworkUrl60+"'>"+value.collectionName+"<"/li>");
-  //  });
-  }); //end of on function
+//$('.stories').html(articles)
+
+$(".ajax-loader").hide();
+
+// .fail(function() {
+//   console.log (fail);
+//   $('stories').append('Sorry! There was a problem, please try again.');
+// };  //end of fail
+
+    });// end of done
+ }); //end of on function
 }); //end of doc.ready
