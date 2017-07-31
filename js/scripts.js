6a8d33589713357e57c1cd51946e7974c29ee020
aleezay <aleezayh@hotmail.com>
2017-07-30 19:07:52 -0700

    called image using css intead of html in js file

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
        url += '?' + $.param({'api-key': '53dfced34ab7497ca7331f1f197cb367'
      })
     
-    $('.stories').empty(); //or .remove
+    $('.stories').empty();
 
     $(".ajax-loader").show();   
 
@@ -20,29 +20,20 @@ $(document).ready(function() {
       console.log (data);
 
       var articles = "";
-      var articleWithImage = data.results.filter(function(value){ //data.results.filter?
+      var articleWithImage = data.results.filter(function(value){
                     return value.multimedia.length
       }).slice(0, 12);
 
      $.each(articleWithImage, function(key,value){
 
         articles += '<li class="articles">';
-        articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<img class="image" src="' + value.multimedia[4].url + '" id="news-img"/>' + '</a>';
+        articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<article style="background-image:url(' + value.multimedia[4].url + ')" img class="image">' + '</a>';
         articles += '<p class="abstract">' + value.abstract + '</p>'
         articles += '</li>';
-
-      //  articles += '<ul class="news">';
-      //  articles += '<a href=' + value.url + '>';
-      //  articles += '<div class="article" style="background-image: url(' + value.multimedia[4].url + ')">';
-      //  articles += '<p class="abstract">' + value.abstract + '</p>'
-      //  articles += '</p></div></a></ul>';    
-      // });
 
      });//end of .each
 
 $('.stories').append(articles);
-
-//$('.stories').html(articles)
 
 $(".ajax-loader").hide();
 
