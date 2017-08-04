a80b993f8ca276fc8c0a583563553732f60b1dc2
aleezay <aleezayh@hotmail.com>
2017-08-04 00:12:07 -0700

    media queries working, added styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".ajax-loader").hide();
-  $('.sections').on('change', function() {
-      event.preventDefault();
+  $('.sections').on('change', function () {
+    event.preventDefault();
 
     var select = $('.sections').val();
     var url = 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json';
-       url += '?' + $.param({'api-key': '53dfced34ab7497ca7331f1f197cb367'
-     })
-    
+    url += '?' + $.param({
+      'api-key': '53dfced34ab7497ca7331f1f197cb367'
+    })
+
     $('.stories').empty();
 
-    $(".ajax-loader").show();   
+    $(".ajax-loader").show();
 
     $.ajax({
-      method: 'GET',
-      url: url
-    })
-    .done(function(data) {
-      console.log (data);
+        method: 'GET',
+        url: url
+      })
+      .done(function (data) {
+        console.log(data);
 
-      var articles = "";
-      var articleWithImage = data.results.filter(function(value){
-                    return value.multimedia.length
-      }).slice(0, 12);
+        var articles = "";
+        var articleWithImage = data.results.filter(function (value) {
+          return value.multimedia.length
+        }).slice(0, 12);
 
-     $.each(articleWithImage, function(key,value){
+        $.each(articleWithImage, function (key, value) {
 
-        articles += '<li class="articles">';
-        articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<article style="background-image:url(' + value.multimedia[4].url + ')" img class="image">' + '</a>';
-        articles += '<p class="abstract">' + value.abstract + '</p>'
-        articles += '</li>';
+          articles += '<li class="articles">';
+          articles += '<a href="' + value.url + '" class="news-anchor"  target="_blank">' + '<article style="background-image:url(' + value.multimedia[4].url + ')" img class="image">' + '</a>';
+          articles += '<p class="abstract">' + value.abstract + '</p>'
+          articles += '</li>';
 
-     });//end of .each
+        }); //end of .each
 
-$('.stories').append(articles);
+        $('.stories').append(articles);
 
-$(".ajax-loader").hide();
+        $(".ajax-loader").hide();
 
-// .fail(function() {
-//   console.log (fail);
-//   $('stories').append('Sorry! There was a problem, please try again.');
-// };  //end of fail
+        // .fail(function() {
+        //   console.log (fail);
+        //   $('stories').append('Sorry! There was a problem, please try again.');
+        // };  //end of fail
 
-    });// end of done
- }); //end of on function
+      }); // end of done
+  }); //end of on function
 }); //end of doc.ready
